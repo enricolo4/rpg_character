76921e20468ea6e324c07970a1e57bb49f1303a9
Enrico <enrico@cora.com.br>
2020-05-06 21:47:38 -0300

    Adicionando verificacao de forma toma no cu

--- domain/service/character/character_service.py
+++ domain/service/character/character_service.py
@@ -7,4 +7,7 @@ class CharacterService:
         self.__character_repository = CharacterRepository()
 
     def save(self, character: Character) -> Character:
+        if character.strength > 100:
+            print("TOMA NO CU")
+
         return self.__character_repository.save(character)
